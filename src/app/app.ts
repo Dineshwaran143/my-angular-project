4daae9b3bec0726f13ebca3d2c26ca8da334e2e1
Dineshwaran A <113098915+Dineshwaran143@users.noreply.github.com>
2025-12-31 10:06:21 +0530

    intiall commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,51 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Target } from '@angular/compiler';
+import { Component, effect, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-angular');
+
+  userName:string ='';
+  count:number =0;
+  value:WritableSignal<number> = signal(10)
+
+  constructor(){
+    effect(()=>{
+      console.log('value of cou t',this.value())
+    })
+  }
+
+  users =['dinesh','femila','vanitha','vicky','andrason']
+
+  valueChange(val:Event){
+    this.userName =(val.target as HTMLInputElement).value
+    console.log((val.target as HTMLInputElement).value)
+  }
+
+  setValue(){
+    this.userName = 'dinesh'
+  }
+  getValue(val:string){
+    this.userName =val;
+    console.log(val)
+  }
+
+  //update count
+
+  updateCount(){
+    this.count++;
+  }
+
+
+  //siganls
+
+  updateSignalValue(){
+    // this.value.set(this.value()+3)
+    this.value.update(val => val +10)
+  }
 }
